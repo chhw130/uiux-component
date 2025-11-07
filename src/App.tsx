1006d7f6c716188f571b9ffc6c4ab9fccf87188f
chhw130 <527coco@naver.com>
2025-11-07 22:07:55 +0900

    feat : multi value state

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import './App.css'
 import { Accordion } from './components/accordion'
 
 const EXAMPLE = {
-  accordion: {
-    title: 'accordion',
+  accordion: Array.from({ length: 5 }, (_, i) => ({
+    title: `accordion ${i + 1}`,
     content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
       Donec euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, 
       quis aliquam nisl nisl eu nisl. 
@@ -12,7 +12,7 @@ const EXAMPLE = {
       Donec euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, 
       quis aliquam nisl nisl eu nisl. 
       Donec euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc.`,
-  },
+  })),
 }
 
 function App() {
@@ -21,14 +21,18 @@ function App() {
       <section>
         <h2>Accordion</h2>
         <Accordion
-          onValueChange={(isOpen: boolean) => {
-            console.log('value change', isOpen)
+          onValueChange={(value) => {
+            console.log('value change', value)
           }}
         >
-          <Accordion.Header>{EXAMPLE.accordion.title}</Accordion.Header>
-          <Accordion.Content>
-            <p>{EXAMPLE.accordion.content}</p>
-          </Accordion.Content>
+          {EXAMPLE.accordion.map((item, idx) => (
+            <Accordion.Item value={idx} key={idx}>
+              <Accordion.Header>{item.title}</Accordion.Header>
+              <Accordion.Content>
+                <p>{item.content}</p>
+              </Accordion.Content>
+            </Accordion.Item>
+          ))}
         </Accordion>
       </section>
     </>
