23dc776ade1deef41d0afe4824d26c1a2670645e
chhw130 <527coco@naver.com>
2025-11-07 19:41:34 +0900

    feat : accordion basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import './App.css'
+import { Accordion } from './components/accordion'
 
 function App() {
-  return <></>
+  return (
+    <>
+      <section>
+        <h2>Accordion</h2>
+        <Accordion>
+          <Accordion.Header>Accordion Item 1</Accordion.Header>
+          <Accordion.Content>
+            <p>Accordion Content</p>
+          </Accordion.Content>
+        </Accordion>
+      </section>
+    </>
+  )
 }
 
 export default App
