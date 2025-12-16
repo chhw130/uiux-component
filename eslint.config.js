eb282bdb5e669d32878f426b78332b8ca3998fab
chhw130 <527coco@naver.com>
2025-12-16 15:29:49 +0900

    chore : fix type checker

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,7 @@ export default defineConfig([
       globals: globals.browser,
     },
     rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
       'react-hooks/exhaustive-deps': 'off',
       'react-refresh/only-export-components': 'off',
     },
